90c8b485a10cebe49f08f3342a73ebac96530e59
avery <averyinthevalley@gmail.com>
2018-11-27 01:38:08 -0800

    Initial Commit

---
+++ HelloWorld1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld1
+//
+//  Created by Yuka on 2018/11/27.
+//  Copyright © 2018年 lychee2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
